3f8e9a60fc5350fe9c4a0b3408a73525c182fb5f
mlaflam <mlaflam@users.noreply.github.com>
2024-01-05 16:37:25 -0500

    initial error page

---
+++ src/components/ErrorFooter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../style.css';
+
+const ErrorFooter = () => {
+  return (
+    <footer className="error-footer">
+      footies
+    </footer>
+  )
+}
+
+export default ErrorFooter
--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../style.css';
 import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import ErrorFooter from '../components/ErrorFooter';
 import { Link } from "react-router-dom";
 
 import leafIconThree from '../icons/leaf-icon-3.png';
@@ -12,30 +12,37 @@ const ErrorPage = () => {
 
       <div>
         <div className="block-main">
-          <Navbar />
+        <Navbar />
 
-          <div className="block">
+        <div className="block">
 
-            <div id="search-box-container">
-              <div className="explore-container">
-                <img className="explore-icon" src={leafIconThree} alt="Plant Preview" />
-                <div className="explore-title">Error 404</div>
-              </div>
-
-              <div className="explore-info">
-                Sorry, the page you were looking for does not exist. 
-            </div>
-            <div className="link-back">
-                <Link to='/' className='title-link' >Back to main</Link>
+          <div id="error-main-container">
+            <div className="error-container">
+              <img className="explore-icon" src={leafIconThree} alt="Plant Preview" />
+              <div className="explore-title">Error 404</div>
             </div>
 
-
-
+            <div className="error-info-first">
+              Oops! Page not found.
             </div>
+            <div className="error-info-second">
+              The page you are looking for might have been
+              removed  <br /> had its name changed or is temporarily unavailable.
+            </div>
+
+            <div className='link-container'>
+              <Link to='/' className='link-back-1'> Back to main</Link>
+
+              <Link to='/' className='link-back-2'> Contact Us</Link>
+            </div>
+
+
 
           </div>
 
-          <Footer />
+        </div>
+
+        <ErrorFooter />
 
         </div>
 
