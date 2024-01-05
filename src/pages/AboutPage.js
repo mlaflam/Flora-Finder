51636ea3b27ca032d31bc22da4ecabc051309ca6
mlaflam <mlaflam@users.noreply.github.com>
2024-01-05 01:19:39 -0500

    editing contact form

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -93,37 +93,38 @@ function AboutPage() {
           <div className='about-page-contact-section' >
 
 
-            <div className="section-container">
+            <div className="section-container-two">
               <img className="contact-section-icon" src={mailIcon} alt="Mail Icon" />
               <div className="section-title">Contact Us</div>
             </div>
 
-            <div className='contact-form-block'>
+            <div className='contact-block'>
+              <div className='contact-form-block'>
 
-              <div className='contact-form-top-block'>
-                <div className='contact-form-top-block-two'>
-                  <div className='contact-form-inner-block'>
-                    <div className='contact-info'>Full Name</div>
-                    <input className='name-info' type="text" placeholder="Name" />
-                  </div>
-                  <div className='contact-form-inner-block'>
-                    <div className='contact-info'>Email Address</div>
-                    <input className='email-info' type="text" placeholder="Email" />
-                  </div>
+                <div className='contact-form-top-block'>
+                  <div className='contact-form-top-block-two'>
+                    <div className='contact-form-inner-block'>
+                      <div className='contact-info'>Full Name</div>
+                      <input className='name-info' type="text" placeholder="Name" />
+                    </div>
+                    <div className='contact-form-inner-block'>
+                      <div className='contact-info'>Email Address</div>
+                      <input className='email-info' type="text" placeholder="Email" />
+                    </div>
 
+                  </div>
                 </div>
-              </div>
 
-              <div>
-                <div className='contact-info'>Message</div>
-                <textarea className='message-info' placeholder="Message" />
-              </div>
+                <div className='contact-form-bottom-block'>
+                  <div className='contact-info'>Message</div>
+                  <textarea className='message-info' placeholder="Message" />
+                </div>
 
-              <div className='button-block'>
-                <button className='contact-button'>Send Message</button>
+                <div className='button-block'>
+                  <button className='contact-button'>Send Message</button>
+                </div>
+              
               </div>
-              
-              
 
             </div>
 
