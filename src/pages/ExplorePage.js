a7ee9d05ef15002b1affe25c9847ae80d661746f
mlaflam <mlaflam@users.noreply.github.com>
2024-01-06 14:22:53 -0500

    Updating  search based on enter key

--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -1,3 +1,4 @@
+import { useEffect, useState, onKeyUp } from "react";
 import '../style.css';
 import ExamplePlant from '../components/Example-Plant';
 import Navbar from '../components/Navbar';
@@ -5,8 +6,43 @@ import compassIcon from '../icons/compass-icon-4.png';
 import searchIcon from '../icons/search-icon-3.png';
 import Footer from '../components/Footer';
 
+const API_URL = 'https://www.omdbapi.com?apikey=e0340b1'
 
 function ExplorePage() {
+  const validStates = ["Alabama", "Alaska", "Arizona", "Arkansas", "California",
+    "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii",
+    "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine",
+    "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
+    "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico",
+    "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon",
+    "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee",
+    "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia",
+    "Wisconsin", "Wyoming"
+ ]
+  
+
+  const [state, setState] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchState = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setState(data.Search);
+    console.log("sucess?")
+  }
+
+  useEffect(() => {
+    searchState('');
+  }, []);
+
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      searchState(searchTerm);
+      {/* console.log("enter pressed") */ }
+    }
+  };
+
   return (
 
     <div>
@@ -27,7 +63,11 @@ function ExplorePage() {
 
             <div id="search-container">
               <img className="search-icon" src={searchIcon} alt="Search Icon" />
-              <input className="search" type="text" placeholder="Enter state" />
+              <input id='search' className="search" type="text" placeholder="Enter state"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                onKeyDown={handleKeyPress}
+                 />
             </div>
 
           </div>
