320e44730db7cffc2fd6838876032c71f6fe73bc
mlaflam <mlaflam@users.noreply.github.com>
2024-01-05 22:10:11 -0500

    creating notification for message sucess and failure

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,14 @@
 import React, { useRef } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import emailjs from '@emailjs/browser';
 import '../style.css';
 import mailIcon from '../icons/mail-icon-1.png';
 
 const Contact = () => {
   const form = useRef();
+
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -13,14 +17,24 @@ const Contact = () => {
       .then((result) => {
         console.log(result.text);
         console.log("message sent!")
+        toast.success("Message Sent!");
       }, (error) => {
         console.log(error.text);
-        console.log("you fail")
+        console.log("message failed to send")
+        toast.error("Message failed to send");
       });
+  }; 
+
+  const clearInput = () => {
+    document.getElementById("info-1").value = "";
+    document.getElementById("info-2").value = "";
+    document.getElementById("info-3").value = "";
   };
+
 
   return (
     <form ref={form} onSubmit={sendEmail} className="section-block">
+      
       <div className='about-page-contact-section' >
 
 
@@ -36,11 +50,11 @@ const Contact = () => {
               <div className='contact-form-top-block-two'>
                 <div className='contact-form-inner-block'>
                   <div className='contact-info'>Full Name</div>
-                  <input className='name-info' placeholder="Name" type="text" name="user_name" />
+                  <input className='name-info' id='info-1' placeholder="Name" type="text" name="user_name" />
                 </div>
                 <div className='contact-form-inner-block'>
                   <div className='contact-info'>Email Address</div>
-                  <input className='email-info' placeholder="Email" type="email" name="user_email" />
+                  <input className='email-info' id='info-2' placeholder="Email" type="email" name="user_email" />
                 </div>
 
               </div>
@@ -48,23 +62,25 @@ const Contact = () => {
 
             <div className='contact-form-bottom-block'>
               <div className='contact-info'>Message</div>
-              <textarea className='message-info' placeholder="Message" name="message" />
+              <textarea className='message-info' id='info-3' placeholder="Message" name="message" />
             </div>
 
             <div className='button-block'>
               
-              <input className='contact-button' type="submit" value="Send" placeholder='Send Message' />
+              <input className='contact-button'
+                 value="Send" type='submit' placeholder='Send Message' 
+                onClick={clearInput} />
+              
             </div>
+            
 
           </div>
 
         </div>
 
 
-
-
       </div>
-
+      <ToastContainer />
     </form>
   )
 }
