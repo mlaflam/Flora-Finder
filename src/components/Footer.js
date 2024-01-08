6bfde9d20de71151bd9a3e9bf42e9a2b7eda648b
mlaflam <mlaflam@users.noreply.github.com>
2024-01-08 16:12:00 -0500

    Adding links to footer options

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../style.css';
 
 import gitIcon from '../icons/git-icon.png';
+import { Link } from "react-router-dom";
 
 
 
@@ -11,28 +12,37 @@ const Footer = () => {
       <div className='footer-container'>
         <div className='footer-section'>
           <div className='footer-title'>FloraFinder</div>
+          <div className="footer-content"><Link to='/Explore' className='LINK' >Explore</Link> </div>
 
-          <div className='footer-content'>Explore</div>
-          <div className='footer-content'>About</div>
-          <div className='footer-content'>Sign In</div>
+          <div className="footer-content"><Link to='/About' className='LINK' >About</Link> </div>
+          <div className="footer-content"><Link to='/SignIn' className='LINK' >Sign In</Link> </div>
+
         </div>
 
         <div className='footer-section'>
           <div className='footer-title'>API's and Data</div>
 
-          <div className='footer-content'>NatureServe</div>
-          <div className='footer-content'>Trefle</div>
+          <a className='LINK' href="https://explorer.natureserve.org/" target='_blank'>
+            <div className='footer-content'>NatureServe</div>
+          </a>
+          <a className='LINK' href="https://trefle.io/" target='_blank'>
+            <div className='footer-content'>Trefle</div>
+          </a>
+
         </div>
 
         <div className='footer-section'>
           <div className='footer-title'>Learn More</div>
 
-          <div className='github-container '>
-            <img className="git-icon-footer" src={gitIcon} alt="Git Icon" />
-            <div className='footer-content'>Github</div>
+          <div className='github-container'>
+            <a id="github-container" className='LINK' href="https://github.com/mlaflam/flora-finder-website" target='_blank'>
+              <img className="git-icon-footer" src={gitIcon} alt="Git Icon" />
+              <div className='footer-content'>Github</div>
+            </a>
+            
           </div>
 
-          <div className='footer-content'>Contact Us</div>
+          <div className="footer-content"><Link to='/About#Contact' className='LINK' >Contact Us</Link> </div>
         </div>
       </div>
       
