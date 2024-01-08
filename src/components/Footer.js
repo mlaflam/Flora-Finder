8fa4f95498e7d20e86f18b38e9e9731ac37cef67
mlaflam <mlaflam@users.noreply.github.com>
2024-01-08 15:50:58 -0500

    First finished footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,43 @@
 import React from 'react'
 import '../style.css';
 
+import gitIcon from '../icons/git-icon.png';
+
+
 
 const Footer = () => {
   return (
     <footer className="footer">
-      footies
+      <div className='footer-container'>
+        <div className='footer-section'>
+          <div className='footer-title'>FloraFinder</div>
+
+          <div className='footer-content'>Explore</div>
+          <div className='footer-content'>About</div>
+          <div className='footer-content'>Sign In</div>
+        </div>
+
+        <div className='footer-section'>
+          <div className='footer-title'>API's and Data</div>
+
+          <div className='footer-content'>NatureServe</div>
+          <div className='footer-content'>Trefle</div>
+        </div>
+
+        <div className='footer-section'>
+          <div className='footer-title'>Learn More</div>
+
+          <div className='github-container '>
+            <img className="git-icon-footer" src={gitIcon} alt="Git Icon" />
+            <div className='footer-content'>Github</div>
+          </div>
+
+          <div className='footer-content'>Contact Us</div>
+        </div>
+      </div>
+      
     </footer>
+    
   )
 }
 
