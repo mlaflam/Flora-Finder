98c7c84b74ebe223e04216c97a358fabee161245
mlaflam <mlaflam@users.noreply.github.com>
2024-01-12 23:59:30 -0500

    Plant page styling

--- src/pages/ExamplePlantPage.js
+++ src/pages/ExamplePlantPage.js
@@ -73,7 +73,7 @@ const ExamplePlantPage = () => {
             </div>
 
             <div className='sections-container-outer'>
-              <div className='sections-container-inner'>
+              <div className='sections-container-inner-1'>
                 <div className='plant-page-header-container'>
                   <img className="plant-page-header-icon" src={leafIconThree} alt="Leaf Icon" />
                   <div className='plant-page-header'>Classification</div>
@@ -89,23 +89,29 @@ const ExamplePlantPage = () => {
                     <div className='class-1 '> {plantData.plant.speciesGlobal.otherCommonNames[0]}</div></div>
 
 
-                  <div className='class-k'>Kingdom:
-                    <div className='class-2 '> Plantae </div> </div>
-                  <div className='class-p'>Phylum:
-                    <div className='class-2 '> {plantData.plant.speciesGlobal.phylum} </div> </div>
-                  <div className='class-c'>Class:
-                    <div className='class-2 '>{plantData.plant.speciesGlobal.taxclass}</div></div>
-                  <div className='class-o'>Order:
-                    <div className='class-2 '>{plantData.plant.speciesGlobal.taxorder}</div></div>
-                  <div className='class-f'>Family:
-                    <div className='class-2 '>{plantData.plant.speciesGlobal.family}</div></div>
-                  <div className='class-s'> Genus:
-                    <div className='class-2 '>{plantData.plant.speciesGlobal.genus}</div></div>
+                  <div className='classes-outer-container'>
+
+                    <div class="triangle"></div>
+                    <div className='classes-container'>
+                      <div className='class-k'>Kingdom:
+                        <div className='class-2 '> Plantae </div> </div>
+                      <div className='class-p'>Phylum:
+                        <div className='class-2 '> {plantData.plant.speciesGlobal.phylum} </div> </div>
+                      <div className='class-c'>Class:
+                        <div className='class-2 '>{plantData.plant.speciesGlobal.taxclass}</div></div>
+                      <div className='class-o'>Order:
+                        <div className='class-2 '>{plantData.plant.speciesGlobal.taxorder}</div></div>
+                      <div className='class-f'>Family:
+                        <div className='class-2 '>{plantData.plant.speciesGlobal.family}</div></div>
+                      <div className='class-s'> Genus:
+                        <div className='class-2 '>{plantData.plant.speciesGlobal.genus}</div></div>
+                    </div>
+
+
+                  </div>
 
 
                 </div>
-
-
 
               </div>
 
